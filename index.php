9254bc25619a2c8ef23c9f4bcb05893224b7cf19
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2021-02-27 17:17:27 +0900

    PHP課題1修正版

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
 <?php echo "日本の首都は？";?>
 
 <form method="post">
 <input type="text" name="answer"></input>
+<button type="submit">OK</button>
 </form>
+
+
 <?php
-$answer = filter_input(INPUT_POST, 'answer');
-if($answer==="東京")｛
+$answer = $_POST['answer'];
+if(isset($answer) || $answer==="東京")｛
 echo "正解" .PHP_EOL;
 }else{
   echo "不正解" .PHP_EOL;
 }
+
+?>
