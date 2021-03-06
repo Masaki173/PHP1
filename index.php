cc23ad8853d01f52a068ccae426ef5000d55abcf
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2021-03-06 15:15:55 +0900

    PHP課題1-4

--- index.php
+++ index.php
@@ -5,24 +5,36 @@
   <title>Document</title>
 </head>
 <body>
+ <form method ="post">
+  <select name="you">
+  <option value="グー">グー</option>
+  <option value="チョキ">チョキ</option>
+  <option value="パー">パー</option>
+  </select>
+  <button>じゃんけん！</button>
+  </form>
   <?php 
-  $questions = [
-   "問題" => "日本の首都は？"
-   ];
-   foreach($questions as $key => $question) {
-     ?><h1><?php echo $key.' '.$question . PHP_EOL; ?></h1><?php
-   }
-  $answers = [
-  "回答1" => "大阪",
-  "回答2" => "北海道",
-  "回答3" => "箱根",
-  "回答4" => "東京",
-  ]; 
-  foreach($answers as $key => $answer) {
- ?><p><?php echo $key. ' '.$answer . PHP_EOL;?></p><?php
-  }
+  $you = filter_input(INPUT_POST, "you");
+  ?><p><?php echo "自分: $you" . PHP_EOL; ?></p><?php
+ 
+ $n = mt_rand(1, 3);
+    switch($n) {
+      case 1:
+      echo '相手: グー';
+      $game = ($you == 'グー' ? 'あいこ！' : ($you == 'パー' ? 'あなたの勝利です！' : 'あなたの敗北です。。。'));
+      ?><p><?php echo "$game" . PHP_EOL; ?></p><?php
+      break;
+      case 2:
+      echo '相手: チョキ';
+      $game = ($you == 'チョキ' ? 'あいこ！' : ($you == 'グー' ? 'あなたの勝利です！' : 'あなたの敗北です。。。'));
+      ?><p><?php echo "$game". PHP_EOL; ?></p><?php
+      break;
+      case 3:
+      echo '相手: パー';
+      $game = ($you == 'パー' ? 'あいこ！' : ($you == 'チョキ' ? 'あなたの勝利です！' : 'あなたの敗北です。。。'));
+      ?><p><?php echo "$game" . PHP_EOL; ?></p><?php
+      break;
+   } 
   ?>
-
-
 </body>
 </html>
