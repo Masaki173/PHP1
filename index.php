387e2fd6c10ff046d042b762cfe17b54db16ec9d
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2021-03-08 11:41:46 +0900

    PHP課題1-4修正版3

--- index.php
+++ index.php
@@ -15,22 +15,22 @@
   </form>
   <?php 
   $you = filter_input(INPUT_POST, "you");
-  ?><p><?php echo "自分: $you" . PHP_EOL; ?></p><?php
+  ?><p><?php echo "$you" . PHP_EOL; ?></p><?php
   if ($_SERVER['REQUEST_METHOD'] === 'POST'){
  $n = mt_rand(1, 3);
     switch($n) {
       case 1:
-      echo 'グー';
+      echo '相手: グー';
       $game = ($you == 'グー' ? 'あいこ！' : ($you == 'パー' ? 'あなたの勝利です！' : 'あなたの敗北です。。。'));
       ?><p><?php echo "$game" . PHP_EOL; ?></p><?php
       break;
       case 2:
-      echo 'チョキ';
+      echo '相手: チョキ';
       $game = ($you == 'チョキ' ? 'あいこ！' : ($you == 'グー' ? 'あなたの勝利です！' : 'あなたの敗北です。。。'));
       ?><p><?php echo "$game" . PHP_EOL; ?></p><?php
       break;
       case 3:
-      echo 'パー';
+      echo '相手: パー';
       $game = ($you == 'パー' ? 'あいこ！' : ($you == 'チョキ' ? 'あなたの勝利です！' : 'あなたの敗北です。。。'));
       ?><p><?php echo "$game" . PHP_EOL; ?></p><?php
       break;
