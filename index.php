7365df1fe0fbcf39d7bdc0f118598ce32a9da216
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2021-03-12 17:47:06 +0900

    PHP課題2修正版

--- index.php
+++ index.php
@@ -11,25 +11,27 @@
   <button type="submit">実行</button>
   </form>
   <?php 
-$fizz = $_POST["fizznum"];
-$buzz = $_POST["buzznum"];
-if(!ctype_digit($fizz)){
+  if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+   $fizz = $_POST["fizznum"];
+   $buzz = $_POST["buzznum"];
+   if(!ctype_digit($fizz)){
   ?><p><?php echo "整数値を入力してください" . PHP_EOL;?></p><?php
   return;
 }
-if(!ctype_digit($buzz)){
+  if(!ctype_digit($buzz)){
    ?><p><?php echo "整数値を入力してください" . PHP_EOL;?></p><?php
    return;
  }
- for($i = 1; $i < 100; $i++){
-  if($i%$fizz == 0 && $i%$buzz == 0){
+  for($i = 1; $i < 100; $i++){
+   if($i%$fizz == 0 && $i%$buzz == 0){
   ?><p><?php echo "FizzBuzz $i" . PHP_EOL;?></p><?php
-   }elseif($i%$fizz == 0){
+  }elseif($i%$fizz == 0){
   ?><p><?php echo "Fizz $i" . PHP_EOL;?></p><?php
   }elseif($i%$buzz == 0){
   ?><p><?php echo "Buzz $i" . PHP_EOL;?></p><?php
     }
   }
+}
   ?>
 </body>
 </html>
